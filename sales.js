1a85bda23d8a36e7a36a97776200df38733d39cc
QB-DAYAANNETH\Dany- <dgonzalez75@mail.csuchico.edu>
2016-11-02 18:08:18 -0700

    Receipt is now working

--- sales.js
+++ sales.js
@@ -20,19 +20,17 @@ function addItem()
     // update runningTotal to be its value plus newItem
     var dollars = asCurrency(runningTotal);
     // create a variable called dollars
-    document.getElementById("subtotal").innerHTML=dollars;
-    newItem= document.getElementById("price").value="";
-    setCookie("preTax", runningTotal, 3);
-      console.log(getCookie("preTax"));
-
-
     // call asCurrency() by with the value of runningTotal and assign the return value to dollars
+    document.getElementById("subtotal").innerHTML=dollars;
+    // update the innerHTML of the span with the id "subtotal" to be dollars
 
+    newItem= document.getElementById("price").value="";
+    // update the value of the input with the id "price" to be an empty string
+    setCookie("preTax", runningTotal, 3);
+    // update a cookie called "preTax" with the value of runningTotal
+      console.log(getCookie("preTax"));
   }
 
-  // update the innerHTML of the span with the id "subtotal" to be dollars
-  // update the value of the input with the id "price" to be an empty string
-  // update a cookie called "preTax" with the value of runningTotal
 }
 
 //takes a number and gives a string with the number displayed as USD currency
@@ -41,6 +39,30 @@ function asCurrency(val)
   return "$" + val.toFixed(2);
 
 }
+
+function calculateReceipt()
+//First, create a new function in sales.js called calculateReceipt() but leave it empty for now.
+{
+  var receiptSubtotal = getCookie("preTax");
+  receiptSubtotal = Number(receiptSubtotal);
+/*Now, within your new function, get the preTax cookie and assign it to a variable
+called receiptSubtotal. Make sure to cast it to a number*/
+  var receiptTax = Number(receiptSubtotal * 0.075);
+  //receiptTax = receiptTax.toFixed(2);
+/*Create another variable called receiptTax. Assign it the value of receiptSubtotal
+multiplied by 0.075 (in other words, calculate 7.5% of preTax).*/
+  var receiptTotal = Number(receiptSubtotal + receiptTax);
+  //receiptTotal = receiptTotal.toFixed(2);
+  /*Create a third variable called receiptTotal and assign it to the sum of the subtotal plus
+  the taxes (which you just calculated)*/
+  document.getElementById("sub").innerHTML = asCurrency(receiptSubtotal);
+  document.getElementById("tax").innerHTML = asCurrency(receiptTax);
+  document.getElementById("tot").innerHTML = asCurrency(receiptTotal);
+/*Once you have calculated those values, have your function update the h2 tags
+ with the respective id: sub, tax, and tot*/
+
+}
+
 
 //courtesy of w3schools, from: http://www.w3schools.com/js/js_cookies.asp
 function setCookie(cname, cvalue, exdays) {
